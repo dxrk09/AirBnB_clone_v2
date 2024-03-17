5ce5f61d08b21029384578a135088815d7ff8957
HajarIH <languages0701@gmail.com>
2024-03-17 15:06:56 +0000

    mysql dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,8 +5,17 @@
 -- GRANT hbnb_dev all privileges on the database hbnb_dev_db
 -- GRANT hbnb_dev SELECT privilege on the database performance_schema
 
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the database if it doesn't exist
 CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY hbnb_dev_pwd;
+
+-- GRANT all privileges to the user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
+
+-- GRANT Select privilege to the user
 GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
+
+--Flush privileges to apply changes
 FLUSH PRIVILEGES;
