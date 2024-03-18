d879f4e514ee8e1c611ccc6fae968ca00e3b5c20
Dxrk <118543407+dxrk09@users.noreply.github.com>
2024-03-18 22:49:50 +0000

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Defines the Review class."""
-from models.base_model import Base, BaseModel
-from models.base_model import 
-from sqlalchemy import Column, ForeignKey, String
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
