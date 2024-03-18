377a6eed1211daf29f3ae9d8cfdef37f6c90f7bf
Dxrk <118543407+dxrk09@users.noreply.github.com>
2024-03-18 22:49:35 +0000

    Update place.py

--- models/place.py
+++ models/place.py
@@ -2,10 +2,16 @@
 """ PLace Module for HBNB project """
 import models
 from os import getenv
-from models.base_model import Base, BaseModel
+from models.base_model import Base
+from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.review import Review
-from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table
+from sqlalchemy import Column
+from sqlalchemy import Float
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Table
 from sqlalchemy.orm import relationship
 
 
