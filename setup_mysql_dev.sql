0a089d9f98365c26718f3f0d0e2437d39e201c4e
Dxrk09 <kanekisenpai703@gmail.com>
2024-03-19 13:08:18 -0400

    Hot Fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,21 +1,5 @@
--- script that prepares a MySQL server
--- database hbnb_dev_db
--- new user hbnb_dev in localhost
--- password of hbnb_dev is hbnb_dev_pwd
--- GRANT hbnb_dev all privileges on the database hbnb_dev_db
--- GRANT hbnb_dev SELECT privilege on the database performance_schema
-
--- Create the database if it doesn't exist
+-- Prepares a MySQL server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create the database if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- GRANT all privileges to the user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- GRANT Select privilege to the user
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
---Flush privileges to apply changes
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,19 +1,5 @@
--- a script that prepares a MySQL server
--- database hbnb_test_db
--- new user hbnb_test
--- password of hbnb_test is hbnb_test_pwd
-
--- Creates hbnb_test_db database
+-- Prepares a MySQL Test server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
--- Creates a new user
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- Grants privileges to new user
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
--- Grants SELECT privilege to new user
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
