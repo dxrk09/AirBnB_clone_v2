a9658514abbe8a04edefc4a4f42376169b9603f2
Dxrk <118543407+dxrk09@users.noreply.github.com>
2024-03-19 22:22:44 +0000

    Update console.py

--- console.py
+++ console.py
@@ -114,30 +114,25 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-    """ Create an object of any class"""
-    try:
-        if not args:
-            raise SyntaxError()
-        arg_list = args.split(" ")
-        kw = {}
-        for arg in arg_list[1:]:
-            arg_splited = arg.split("=")
-            arg_splited[1] = eval(arg_splited[1])
-            if type(arg_splited[1]) is str:
-                arg_splited[1] = arg_splited[1].replace("_", " ").replace('"', '\\"')
-            kw[arg_splited[0]] = arg_splited[1]
-        class_name = arg_list[0]
-        if class_name not in HBNBCommand.classes:
-            raise NameError()
-    except SyntaxError:
-        print("** class name missing **")
-    except NameError:
-        print("** class doesn't exist **")
-    else:
-        new_instance = HBNBCommand.classes[class_name](**kw)
+        """ Create an object of any class"""
+        try:
+            if not args:
+                raise SyntaxError()
+            arg_list = args.split(" ")
+            kw = {}
+            for arg in arg_list[1:]:
+                arg_splited = arg.split("=")
+                arg_splited[1] = eval(arg_splited[1])
+                if type(arg_splited[1]) is str:
+                    arg_splited[1] = arg_splited[1].replace("_", " ").replace('"', '\\"')
+                kw[arg_splited[0]] = arg_splited[1]
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
+        new_instance = HBNBCommand.classes[arg_list[0]](**kw)
         new_instance.save()
         print(new_instance.id)
-
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
