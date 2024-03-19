d95b5ad92abdfa6295709205e7d7cd2a2a7e0e3d
Dxrk <118543407+dxrk09@users.noreply.github.com>
2024-03-19 21:56:19 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 """Defines the User class."""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import Place
 from models.review import Review
